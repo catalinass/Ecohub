d7c81b124684ffc3c498571f55d654eb39029a28
Sanchez Vargas <catalina.sanchez.vargas@intel.com>
2019-07-30 16:20:51 -0600

    last commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,22 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import {AssetsService } from '../services/assets.service';
 import {Router} from '@angular/router';
 
+interface RaspData{
+  peso: number;
+  eco: number;
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  
+  
+  data: RaspData;
+  
   constructor(private router: Router, private assetsService: AssetsService) { }
+  
+  
 
   ngOnInit() {
   }
 
   start(){
    this.assetsService.getWeight().subscribe(
-     response => {
-     console.log(response)
+   (response: RaspData) => {
+     this.data = response;
+     //console.log(response)
      }
    )
 
@@ -27,5 +39,5 @@ export class HomeComponent implements OnInit {
      this.router.navigate(['/login']);
   }
   
-
+  
 }
